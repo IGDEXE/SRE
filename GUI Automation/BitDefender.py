ba53ba9c784b3ba86897d94d11245efad435238d
Ivo Dias <igd753@gmail.com>
2019-05-08 11:03:03 -0300

    Py

---
+++ GUI Automation/BitDefender.py
@@ -0,0 +1,9 @@
+# Importa as blibliotecas
+import pyautogui
+import time
+
+# Abre o executar do Windows e busca o servidor
+pyautogui.hotkey('win','r');pyautogui.typewrite('\\\srsdcsp01\\NETLOGON\\Bitdefender\\Setup');pyautogui.press('enter')
+time.sleep(2)
+# Abre o arquivo de instalação
+pyautogui.click(520,479);pyautogui.typewrite('epskit_x64');pyautogui.press('enter')
